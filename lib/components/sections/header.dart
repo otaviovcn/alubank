2a1af91c1695cae4a05bae96af579b75e4bc1b22
otaviovcn <viinident@gmail.com>
2025-08-04 15:00:52 -0300

    :lipstick: feat: Styles header section

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -6,16 +6,48 @@ class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              Text('\$1000.00'),
-              Text('Balanço disponível'),
-            ],
-          ),
-          Icon(Icons.account_circle)
-        ],
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color>[
+            Color.fromRGBO(103, 99, 234, 1),
+            Color.fromRGBO(155, 105, 254, 1),
+            Color.fromRGBO(195, 107, 255, 1),
+          ],
+        ),
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10))
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 80.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text.rich(
+                  TextSpan(
+                    text: '\$',
+                    style: TextStyle(fontSize: 16),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: '1000.00',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Text('Balanço disponível', style: TextStyle(fontSize: 16)),
+              ],
+            ),
+            Icon(Icons.account_circle, size: 42),
+          ],
+        ),
       ),
     );
   }
