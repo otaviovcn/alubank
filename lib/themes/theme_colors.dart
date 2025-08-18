a021651ba6c47e8a59dbb4842cdc4e8fdf3fcfac
otaviovcn <viinident@gmail.com>
2025-08-18 14:59:07 -0300

    :lipstick: feat: Creates default colors theme

---
+++ lib/themes/theme_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const MaterialColor primaryColor = Colors.purple;
+  static const List<Color> headerGradient = [
+    Color.fromRGBO(103, 99, 234, 1),
+    Color.fromRGBO(155, 105, 254, 1),
+    Color.fromRGBO(195, 107, 255, 1),
+  ];
+  static const Color cardColorDark = Color.fromRGBO(66, 66, 66, 1);
+  static const Color backgroundColorDark = Color.fromRGBO(48, 48, 48, 1);
+}
